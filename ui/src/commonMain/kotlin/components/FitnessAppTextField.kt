6176a5cf8e8758ccb9b87aba83f72c47dee5ccaf
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-04-27 14:29:52 +0200

    Add correct colors to fields with error

--- ui/src/commonMain/kotlin/components/FitnessAppTextField.kt
+++ ui/src/commonMain/kotlin/components/FitnessAppTextField.kt
@@ -1,6 +1,9 @@
 package components
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,6 +12,8 @@ import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.VisualTransformation
@@ -24,6 +29,31 @@ fun FitnessAppTextField(
     leadingIcon: Icon? = null,
     label: String
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+
+    val isError = textFieldData.error != null
+    val focusedColor by animateColorAsState(
+        targetValue = if (isError) FitnessAppTheme.colors.error else FitnessAppTheme.colors.contentPrimary
+    )
+    val unfocusedColor by animateColorAsState(
+        targetValue = if (isError) FitnessAppTheme.colors.error else FitnessAppTheme.colors.contentSecondary
+    )
+    val labelColor by animateColorAsState(
+        targetValue = if (isError && textFieldData.text.isNotEmpty()) {
+            FitnessAppTheme.colors.error
+        } else {
+            FitnessAppTheme.colors.contentSecondary
+        }
+    )
+    val iconColor by animateColorAsState(
+        targetValue = if (isFocused) {
+            FitnessAppTheme.colors.contentPrimary
+        } else {
+            FitnessAppTheme.colors.contentSecondary
+        }
+    )
+
     Column(
         modifier = Modifier.animateContentSize(),
         verticalArrangement = Arrangement.spacedBy(2.dp)
@@ -36,18 +66,22 @@ fun FitnessAppTextField(
             label = { Text(text = label) },
             leadingIcon = {
                 leadingIcon?.let {
-                    FitnessAppIcon(icon = leadingIcon)
+                    FitnessAppIcon(
+                        icon = leadingIcon,
+                        tint = iconColor
+                    )
                 }
             },
             visualTransformation = visualTransformation,
             colors = TextFieldDefaults.outlinedTextFieldColors(
                 textColor = FitnessAppTheme.colors.contentPrimary,
                 cursorColor = FitnessAppTheme.colors.contentPrimary,
-                focusedBorderColor = FitnessAppTheme.colors.contentPrimary,
-                unfocusedBorderColor = FitnessAppTheme.colors.contentSecondary,
-                focusedLabelColor = FitnessAppTheme.colors.contentPrimary,
-                unfocusedLabelColor = FitnessAppTheme.colors.contentSecondary,
-            )
+                focusedBorderColor = focusedColor,
+                unfocusedBorderColor = unfocusedColor,
+                focusedLabelColor = focusedColor,
+                unfocusedLabelColor = labelColor,
+            ),
+            interactionSource = interactionSource
         )
 
         textFieldData.error?.let {
