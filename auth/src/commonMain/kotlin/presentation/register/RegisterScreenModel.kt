8c3304a3f79ab3177568faf083c49eecb8520db1
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-05-02 19:35:38 +0200

    Add test tag to text fields

--- auth/src/commonMain/kotlin/presentation/register/RegisterScreenModel.kt
+++ auth/src/commonMain/kotlin/presentation/register/RegisterScreenModel.kt
@@ -9,13 +9,11 @@ class RegisterScreenModel: BaseModel() {
     val email = MutableStateFlow(TextFieldData())
     val username = MutableStateFlow(TextFieldData())
     val password = MutableStateFlow(TextFieldData())
-    val confirmPassword = MutableStateFlow(TextFieldData())
 
     init {
         email.initTextField()
         username.initTextField()
         password.initTextField()
-        confirmPassword.initTextField()
     }
 
     fun onRegisterButtonClicked() {
---
+++ ui/src/commonMain/kotlin/utils/TestTags.kt
@@ -0,0 +1,7 @@
+package utils
+
+object TestTags {
+    const val EMAIL = "email"
+    const val PASSWORD = "password"
+    const val USERNAME = "username"
+}
