262458be0804b78419cb28f3c2d203f59be03d99
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-11 08:05:43 +0200

    Add favorites icon to log streak tab

--- summary/src/commonMain/kotlin/presentation/components/LogStreakSection.kt
+++ summary/src/commonMain/kotlin/presentation/components/LogStreakSection.kt
@@ -1,9 +1,16 @@
 package presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.gmail.bogumilmecel2.ui.composeResources.Res
@@ -20,20 +27,33 @@ fun LogStreakSection(
     logStreak: Int,
 ) {
     FitnessAppCard(modifier = modifier) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(
-                text = stringResource(Res.string.you_have_logged_for),
-                style = FitnessAppTheme.typography.labelLarge,
-                color = FitnessAppTheme.colors.contentPrimary,
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Favorite,
+                contentDescription = null,
+                tint = FitnessAppTheme.colors.primary,
+                modifier = Modifier.size(24.dp)
             )
 
-            VerticalSpacer(size = 4.dp)
+            Column {
+                Text(
+                    text = stringResource(Res.string.you_have_logged_for),
+                    style = FitnessAppTheme.typography.labelLarge,
+                    color = FitnessAppTheme.colors.contentPrimary,
+                )
 
-            Text(
-                text = stringResource(Res.string.days, logStreak),
-                style = FitnessAppTheme.typography.bodyMedium,
-                color = FitnessAppTheme.colors.contentPrimary,
-            )
+                VerticalSpacer(size = 4.dp)
+
+                Text(
+                    text = stringResource(Res.string.days, logStreak),
+                    style = FitnessAppTheme.typography.bodyMedium,
+                    color = FitnessAppTheme.colors.contentPrimary,
+                )
+            }
         }
     }
 }
