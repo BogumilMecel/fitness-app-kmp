ec1b5abd4ea24f0d05e6aae422bf19e02e4beda5
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-16 19:11:04 +0200

    Add meal

---
+++ diary/src/commonMain/kotlin/domain/model/Meal.kt
@@ -0,0 +1,23 @@
+package domain.model
+
+import androidx.compose.runtime.Composable
+import com.gmail.bogumilmecel2.diary.composeResources.Res
+import com.gmail.bogumilmecel2.diary.composeResources.meal_name_breakfast
+import com.gmail.bogumilmecel2.diary.composeResources.meal_name_dinner
+import com.gmail.bogumilmecel2.diary.composeResources.meal_name_lunch
+import com.gmail.bogumilmecel2.diary.composeResources.meal_name_supper
+import kotlinx.serialization.Serializable
+import org.jetbrains.compose.resources.stringResource
+
+@Serializable
+enum class Meal {
+    BREAKFAST, LUNCH, DINNER, SUPPER;
+
+    @Composable
+    fun getName() = when (this) {
+        BREAKFAST -> stringResource(Res.string.meal_name_breakfast)
+        LUNCH -> stringResource(Res.string.meal_name_lunch)
+        DINNER -> stringResource(Res.string.meal_name_dinner)
+        SUPPER -> stringResource(Res.string.meal_name_supper)
+    }
+}
