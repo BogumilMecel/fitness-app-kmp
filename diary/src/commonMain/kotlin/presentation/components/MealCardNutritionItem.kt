b32cfb5c4a6cc6645bcc91f09797daa326ed1cc1
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-17 07:46:09 +0200

    Add meal card nutrition item

---
+++ diary/src/commonMain/kotlin/presentation/components/MealCardNutritionItem.kt
@@ -0,0 +1,45 @@
+package presentation.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import components.HorizontalProgressIndicator
+import components.HorizontalSpacer
+import theme.FitnessAppTheme
+
+@Composable
+fun MealCardNutritionItem(
+    currentValue: Int,
+    wantedValue: Int,
+    name: String,
+    modifier: Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        HorizontalProgressIndicator(
+            progress = if (wantedValue.toFloat() > 0.0f) (currentValue.toFloat() / wantedValue.toFloat() * 100.0f) else 0.0f
+        )
+
+        HorizontalSpacer(size = 6.dp)
+
+        Column {
+            Text(
+                text = currentValue.toString(),
+                style = FitnessAppTheme.typography.bodyLarge,
+                color = FitnessAppTheme.colors.contentPrimary,
+            )
+
+            Text(
+                text = name,
+                style = FitnessAppTheme.typography.bodyMedium,
+                color = FitnessAppTheme.colors.contentSecondary,
+            )
+        }
+    }
+}
