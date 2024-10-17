0dcf8fb27d4be001d19c595a5e98b721793d8ffb
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-17 07:25:52 +0200

    Add recipe diary entry

---
+++ diary/src/commonMain/kotlin/domain/model/RecipeDiaryEntry.kt
@@ -0,0 +1,51 @@
+package domain.model
+
+import androidx.compose.runtime.Composable
+import com.gmail.bogumilmecel2.diary.composeResources.Res
+import com.gmail.bogumilmecel2.diary.composeResources.servings
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.jetbrains.compose.resources.pluralStringResource
+
+@Serializable
+data class RecipeDiaryEntry(
+    @SerialName("id")
+    override val id: String,
+
+    @SerialName("nutrition_values")
+    override val nutritionValues: NutritionValues,
+
+    @SerialName("date")
+    override val date: LocalDate,
+
+    @SerialName("user_id")
+    override val userId: String,
+
+    @SerialName("meal_name")
+    override val mealName: MealName,
+
+    @SerialName("creation_date")
+    override val creationDateTime: LocalDateTime,
+
+    @SerialName("change_date")
+    override val changeDateTime: LocalDateTime,
+
+    @SerialName("recipe_name")
+    val recipeName: String,
+
+    @SerialName("recipe_id")
+    val recipeId: String,
+
+    @SerialName("servings")
+    val servings: Int,
+
+    @SerialName("deleted")
+    val deleted: Boolean,
+) : DiaryItem {
+    @Composable
+    override fun getDisplayValue() = pluralStringResource(resource = Res.plurals.servings, quantity = servings)
+
+    override fun getDiaryEntryType() = DiaryEntryType.RECIPE
+}
