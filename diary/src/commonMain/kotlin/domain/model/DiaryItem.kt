4af11efb9fc71b7277246c3280bc8a0252fe7e43
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-17 07:10:52 +0200

    Change Diary Item fields to non nullable

--- diary/src/commonMain/kotlin/domain/model/DiaryItem.kt
+++ diary/src/commonMain/kotlin/domain/model/DiaryItem.kt
@@ -5,16 +5,16 @@ import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 
 interface DiaryItem {
-    val id: String?
-    val nutritionValues: NutritionValues?
-    val userId: String?
-    val date: LocalDate?
-    val meal: Meal?
-    val creationDateTime: LocalDateTime?
-    val changeDateTime: LocalDateTime?
+    val id: String
+    val nutritionValues: NutritionValues
+    val userId: String
+    val date: LocalDate
+    val mealName: MealName
+    val creationDateTime: LocalDateTime
+    val changeDateTime: LocalDateTime
 
     @Composable
-    fun getDisplayValue(): String?
+    fun getDisplayValue(): String
 
     fun getDiaryEntryType(): DiaryEntryType
 }
