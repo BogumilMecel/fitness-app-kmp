09ebd4f981fd004af8d4871b40d7fc2be058b9a8
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-20 20:40:49 +0200

    Add get user recipes to api

--- diary/src/commonMain/kotlin/data/api/DiaryApi.kt
+++ diary/src/commonMain/kotlin/data/api/DiaryApi.kt
@@ -3,6 +3,7 @@ import domain.model.Product
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.url
 import io.ktor.http.parameters
 import kotlinx.datetime.LocalDateTime
 
@@ -16,5 +17,18 @@ class DiaryApi(private val httpClient: HttpClient) {
                 )
             }
         }
+        url(urlString = "/userData/products")
+    }.body<List<Product>>()
+
+    suspend fun getUserRecipes(latestDateTime: LocalDateTime?) = httpClient.get {
+        parameters {
+            latestDateTime?.toString()?.let {
+                append(
+                    name = Constants.Query.LATEST_DATE_TIME,
+                    value = it
+                )
+            }
+        }
+        url(urlString = "/userData/recipes")
     }.body<List<Product>>()
 }
