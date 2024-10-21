34a756aaeb669fafca68e5011660a8f0661d99c1
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-21 07:35:06 +0200

    Add all offline methods to diary repository

--- diary/src/commonMain/kotlin/data/repository/DiaryRepositoryImp.kt
+++ diary/src/commonMain/kotlin/data/repository/DiaryRepositoryImp.kt
@@ -1,7 +1,12 @@
 import data.BaseRepository
 import data.api.DiaryDao
+import domain.model.NutritionValues
 import domain.model.Product
+import domain.model.ProductDiaryEntry
+import domain.model.Recipe
+import domain.model.RecipeDiaryEntry
 import domain.repository.DiaryRepository
+import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import utils.Resource
 
@@ -18,8 +23,8 @@ class DiaryRepositoryImp(
     override suspend fun getOfflineProducts(
         userId: String?,
         searchText: String?,
-        limit: Int,
-        skip: Int,
+        limit: Long,
+        skip: Long,
     ): Resource<List<Product>> {
         return handleRequest {
             diaryDao.getProducts(
@@ -41,5 +46,98 @@ class DiaryRepositoryImp(
         return handleRequest {
             diaryDao.insertProduct(product = product)
         }
+    }
+
+    override suspend fun getOfflineProduct(productId: String): Resource<Product?> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getOfflineProductDiaryEntries(
+        searchText: String?,
+        limit: Long,
+        skip: Long
+    ): Resource<List<ProductDiaryEntry>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getOfflineProductDiaryEntries(limit: Long): Resource<List<ProductDiaryEntry>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getOfflineProductDiaryEntries(date: LocalDate): Resource<List<ProductDiaryEntry>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertOfflineProductDiaryEntries(productDiaryEntries: List<ProductDiaryEntry>): Resource<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertOfflineProductDiaryEntry(productDiaryEntry: ProductDiaryEntry): Resource<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteOfflineProductDiaryEntries(date: LocalDate): Resource<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteOfflineProductDiaryEntry(productDiaryEntryId: String): Resource<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getOfflineRecipes(
+        userId: String?,
+        searchText: String?,
+        limit: Long,
+        skip: Long
+    ): Resource<List<Recipe>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getOfflineRecipe(recipeId: String): Resource<Recipe?> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertOfflineRecipes(recipes: List<Recipe>): Resource<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertOfflineRecipe(recipe: Recipe): Resource<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getOfflineRecipeDiaryEntries(
+        searchText: String?,
+        limit: Long,
+        skip: Long
+    ): Resource<List<RecipeDiaryEntry>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getOfflineRecipeDiaryEntries(limit: Long): Resource<List<RecipeDiaryEntry>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getOfflineRecipeDiaryEntries(date: LocalDate): Resource<List<RecipeDiaryEntry>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertOfflineRecipeDiaryEntries(recipeDiaryEntries: List<RecipeDiaryEntry>): Resource<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertOfflineRecipeDiaryEntry(recipeDiaryEntry: RecipeDiaryEntry): Resource<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteOfflineRecipeDiaryEntries(date: LocalDate): Resource<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteOfflineRecipeDiaryEntry(recipeDiaryEntryId: String): Resource<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getOfflineDiaryEntriesNutritionValues(date: LocalDate): Resource<List<NutritionValues>> {
+        TODO("Not yet implemented")
     }
 }
--- diary/src/commonMain/kotlin/domain/repository/DiaryRepository.kt
+++ diary/src/commonMain/kotlin/domain/repository/DiaryRepository.kt
@@ -1,6 +1,11 @@
 package domain.repository
 
+import domain.model.NutritionValues
 import domain.model.Product
+import domain.model.ProductDiaryEntry
+import domain.model.Recipe
+import domain.model.RecipeDiaryEntry
+import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import utils.Resource
 
@@ -10,11 +15,64 @@ interface DiaryRepository {
     suspend fun getOfflineProducts(
         userId: String? = null,
         searchText: String? = null,
-        limit: Int,
-        skip: Int = 0,
+        limit: Long,
+        skip: Long = 0,
     ): Resource<List<Product>>
+
+    suspend fun getOfflineProduct(productId: String): Resource<Product?>
 
     suspend fun insertOfflineProducts(products: List<Product>): Resource<Unit>
 
     suspend fun insertOfflineProduct(product: Product): Resource<Unit>
+
+    suspend fun getOfflineProductDiaryEntries(
+        searchText: String? = null,
+        limit: Long,
+        skip: Long = 0,
+    ): Resource<List<ProductDiaryEntry>>
+
+    suspend fun getOfflineProductDiaryEntries(limit: Long): Resource<List<ProductDiaryEntry>>
+
+    suspend fun getOfflineProductDiaryEntries(date: LocalDate): Resource<List<ProductDiaryEntry>>
+
+    suspend fun insertOfflineProductDiaryEntries(productDiaryEntries: List<ProductDiaryEntry>): Resource<Unit>
+
+    suspend fun insertOfflineProductDiaryEntry(productDiaryEntry: ProductDiaryEntry): Resource<Unit>
+
+    suspend fun deleteOfflineProductDiaryEntries(date: LocalDate): Resource<Unit>
+
+    suspend fun deleteOfflineProductDiaryEntry(productDiaryEntryId: String): Resource<Unit>
+
+    suspend fun getOfflineRecipes(
+        userId: String? = null,
+        searchText: String? = null,
+        limit: Long,
+        skip: Long = 0,
+    ): Resource<List<Recipe>>
+
+    suspend fun getOfflineRecipe(recipeId: String): Resource<Recipe?>
+
+    suspend fun insertOfflineRecipes(recipes: List<Recipe>): Resource<Unit>
+
+    suspend fun insertOfflineRecipe(recipe: Recipe): Resource<Unit>
+
+    suspend fun getOfflineRecipeDiaryEntries(
+        searchText: String? = null,
+        limit: Long,
+        skip: Long = 0,
+    ): Resource<List<RecipeDiaryEntry>>
+
+    suspend fun getOfflineRecipeDiaryEntries(limit: Long): Resource<List<RecipeDiaryEntry>>
+
+    suspend fun getOfflineRecipeDiaryEntries(date: LocalDate): Resource<List<RecipeDiaryEntry>>
+
+    suspend fun insertOfflineRecipeDiaryEntries(recipeDiaryEntries: List<RecipeDiaryEntry>): Resource<Unit>
+
+    suspend fun insertOfflineRecipeDiaryEntry(recipeDiaryEntry: RecipeDiaryEntry): Resource<Unit>
+
+    suspend fun deleteOfflineRecipeDiaryEntries(date: LocalDate): Resource<Unit>
+
+    suspend fun deleteOfflineRecipeDiaryEntry(recipeDiaryEntryId: String): Resource<Unit>
+
+    suspend fun getOfflineDiaryEntriesNutritionValues(date: LocalDate): Resource<List<NutritionValues>>
 }
