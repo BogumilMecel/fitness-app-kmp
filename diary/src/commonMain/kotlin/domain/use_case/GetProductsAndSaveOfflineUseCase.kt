347d63c04d8d4b8e55a39c6f89a7c5a983791633
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-21 07:31:22 +0200

    Add GetProductsAndSaveOfflineUseCase

---
+++ diary/src/commonMain/kotlin/domain/use_case/GetProductsAndSaveOfflineUseCase.kt
@@ -0,0 +1,24 @@
+package domain.use_case
+
+import domain.repository.DiaryRepository
+import domain.services.SettingsService
+import kotlinx.coroutines.flow.firstOrNull
+import utils.Resource
+
+class GetProductsAndSaveOfflineUseCase(
+    private val diaryRepository: DiaryRepository,
+    private val settingsService: SettingsService,
+) {
+    suspend operator fun invoke(): Resource<Unit> {
+        val latestOfflineProduct = diaryRepository.getOfflineProducts(
+            userId = settingsService.getUser().firstOrNull()?.id,
+            limit = 1,
+        ).data ?: return Resource.Error()
+
+        val userProducts = diaryRepository.getUserProducts(
+            latestDateTime = latestOfflineProduct.firstOrNull()?.creationDateTime
+        ).data ?: return Resource.Error()
+
+        return diaryRepository.insertOfflineProducts(userProducts)
+    }
+}
