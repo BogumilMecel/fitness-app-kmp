68ae420a27c9ee724a5b2586de464497de2fb3eb
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-21 07:55:10 +0200

    Add diary entries response

---
+++ diary/src/commonMain/kotlin/domain/model/DiaryEntriesResponse.kt
@@ -0,0 +1,9 @@
+package domain.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DiaryEntriesResponse(
+    val productDiaryEntries: List<ProductDiaryEntry> = emptyList(),
+    val recipeDiaryEntries: List<RecipeDiaryEntry> = emptyList()
+)
