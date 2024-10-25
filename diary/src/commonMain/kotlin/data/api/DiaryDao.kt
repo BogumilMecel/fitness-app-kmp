1572aef2856be19149ebe0152180685327194655
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-25 07:48:50 +0200

    Add recipe diary entry dao functions

--- diary/src/commonMain/kotlin/data/api/DiaryDao.kt
+++ diary/src/commonMain/kotlin/data/api/DiaryDao.kt
@@ -9,6 +9,7 @@ import domain.model.NutritionValues
 import domain.model.Product
 import domain.model.ProductDiaryEntry
 import domain.model.Recipe
+import domain.model.RecipeDiaryEntry
 import kotlinx.coroutines.flow.Flow
 import kotlinx.datetime.LocalDate
 
@@ -81,4 +82,37 @@ interface DiaryDao {
     // Get recipe by ID
     @Query("SELECT * FROM Recipe WHERE id = :recipeId LIMIT 1")
     suspend fun getRecipe(recipeId: String): Recipe?
+
+    // Insert single recipe diary entry
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipeDiaryEntry(entry: RecipeDiaryEntry)
+
+    // Get recipe diary entries with search
+    @Query("""
+        SELECT * FROM RecipeDiaryEntry
+        WHERE (:searchText IS NULL OR recipe_name LIKE '%' || :searchText || '%')
+        GROUP BY recipe_id
+        ORDER BY creationDateTime DESC LIMIT :limit OFFSET :offset
+    """)
+    suspend fun getRecipeDiaryEntries(searchText: String?, limit: Int, offset: Int): List<RecipeDiaryEntry>
+
+    // Get all recipe diary entries
+    @Query("SELECT * FROM RecipeDiaryEntry ORDER BY creationDateTime DESC LIMIT :limit")
+    suspend fun getAllRecipeDiaryEntries(limit: Int): List<RecipeDiaryEntry>
+
+    // Get recipe diary entries by date
+    @Query("SELECT * FROM RecipeDiaryEntry WHERE date = :date ORDER BY creationDateTime DESC")
+    suspend fun getRecipeDiaryEntriesByDate(date: LocalDate): List<RecipeDiaryEntry>
+
+    // Delete a recipe diary entry
+    @Delete
+    suspend fun deleteRecipeDiaryEntry(entry: RecipeDiaryEntry)
+
+    // Delete recipe diary entries by date
+    @Query("DELETE FROM RecipeDiaryEntry WHERE date = :date")
+    suspend fun deleteRecipeDiaryEntriesByDate(date: LocalDate)
+
+    // Get recipe diary entries' nutrition values by date
+    @Query("SELECT nutrition_values FROM RecipeDiaryEntry WHERE date = :date")
+    suspend fun getRecipeDiaryEntriesNutritionValues(date: LocalDate): List<NutritionValues>
 }
