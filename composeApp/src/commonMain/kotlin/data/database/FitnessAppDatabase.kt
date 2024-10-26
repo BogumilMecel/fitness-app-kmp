729379a42fac26ccce4bcc408c59d21c22bd0dec
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-26 15:56:44 +0200

    Add type converter to nutrition values

--- composeApp/src/commonMain/kotlin/data/database/FitnessAppDatabase.kt
+++ composeApp/src/commonMain/kotlin/data/database/FitnessAppDatabase.kt
@@ -2,13 +2,19 @@ package data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import data.api.DiaryDao
+import domain.model.NutritionValuesTypeConverter
 import domain.model.Product
+import domain.model.ProductDiaryEntry
+import domain.model.Recipe
+import domain.model.RecipeDiaryEntry
 
 @Database(
-    entities = [Product::class],
+    entities = [Product::class, Recipe::class, ProductDiaryEntry::class, RecipeDiaryEntry::class],
     version = 1,
 )
+@TypeConverters(value = [NutritionValuesTypeConverter::class])
 abstract class FitnessAppDatabase : RoomDatabase() {
 
     companion object {
--- shared/src/commonMain/kotlin/domain/model/NutritionValues.kt
+++ shared/src/commonMain/kotlin/domain/model/NutritionValues.kt
@@ -1,22 +1,34 @@
 package domain.model
 
+import androidx.room.TypeConverter
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 @Serializable
 data class NutritionValues(
     @SerialName("calories")
-    val calories: Int,
+    val calories: Int = 0,
 
     @SerialName("carbohydrates")
-    val carbohydrates: Double,
+    val carbohydrates: Double = 0.0,
 
     @SerialName("protein")
-    val protein: Double,
+    val protein: Double = 0.0,
 
     @SerialName("fat")
-    val fat: Double,
+    val fat: Double = 0.0,
 )
+
+class NutritionValuesTypeConverter {
+
+    @TypeConverter
+    fun fromNutritionValues(value: NutritionValues): String = Json.encodeToString(value)
+
+    @TypeConverter
+    fun toNutritionValues(value: String): NutritionValues = Json.decodeFromString(value)
+}
 
 fun NutritionValues.multiplyBy(number: Double): NutritionValues {
     return NutritionValues(
