21e407e1a29a50ad9bc97d618c0be61106b2a877
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-26 17:17:58 +0200

    Work on diary dao functions

--- diary/src/commonMain/kotlin/data/api/DiaryDao.kt
+++ diary/src/commonMain/kotlin/data/api/DiaryDao.kt
@@ -5,7 +5,6 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import domain.model.NutritionValues
 import domain.model.Product
 import domain.model.ProductDiaryEntry
 import domain.model.Recipe
@@ -16,13 +15,13 @@ import kotlinx.datetime.LocalDate
 @Dao
 interface DiaryDao {
     @Query("""
-        SELECT * FROM SqlProduct
+        SELECT * FROM Product
         WHERE (:userId IS NULL OR userId = :userId)
         AND (:searchText IS NULL OR name LIKE '%' || :searchText || '%')
         ORDER BY creationDateTime DESC
         LIMIT :limit OFFSET :offset
     """)
-    suspend fun getProducts(
+    fun getProducts(
         userId: String?,
         searchText: String?,
         limit: Long,
@@ -34,25 +33,28 @@ interface DiaryDao {
     suspend fun insertProduct(product: Product)
 
     // Get product by ID
-    @Query("SELECT * FROM SqlProduct WHERE id = :productId LIMIT 1")
+    @Query("SELECT * FROM Product WHERE id = :productId LIMIT 1")
     suspend fun getProduct(productId: String): Product?
 
+    // Insert single product diary entry
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertProductDiaryEntries(productDiaryEntries: List<ProductDiaryEntry>)
 
     // Insert single product diary entry
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertProductDiaryEntry(entry: ProductDiaryEntry)
 
     // Get product diary entries with search
-    @Query("SELECT * FROM ProductDiaryEntry WHERE (:searchText IS NULL OR product_name LIKE '%' || :searchText || '%') GROUP BY product_id ORDER BY creationDateTime DESC LIMIT :limit OFFSET :offset")
+    @Query("SELECT * FROM ProductDiaryEntry WHERE (:searchText IS NULL OR productName LIKE '%' || :searchText || '%') GROUP BY productId ORDER BY creationDateTime DESC LIMIT :limit OFFSET :offset")
     suspend fun getProductDiaryEntries(searchText: String?, limit: Int, offset: Int): List<ProductDiaryEntry>
 
     // Get all product diary entries
     @Query("SELECT * FROM ProductDiaryEntry ORDER BY creationDateTime DESC LIMIT :limit")
-    suspend fun getAllProductDiaryEntries(limit: Int): List<ProductDiaryEntry>
+    fun getAllProductDiaryEntries(limit: Long): Flow<List<ProductDiaryEntry>>
 
     // Get product diary entries by date
     @Query("SELECT * FROM ProductDiaryEntry WHERE date = :date ORDER BY creationDateTime DESC")
-    suspend fun getProductDiaryEntriesByDate(date: LocalDate): Flow<List<ProductDiaryEntry>>
+    fun getProductDiaryEntriesByDate(date: LocalDate): Flow<List<ProductDiaryEntry>>
 
     // Delete a product diary entry
     @Delete
@@ -61,23 +63,27 @@ interface DiaryDao {
     // Delete product diary entries by date
     @Query("DELETE FROM ProductDiaryEntry WHERE date = :date")
     suspend fun deleteProductDiaryEntriesByDate(date: LocalDate)
-
-    // Get product diary entries' nutrition values by date
-    @Query("SELECT nutritionValues FROM ProductDiaryEntry WHERE date = :date")
-    suspend fun getProductDiaryEntriesNutritionValues(date: LocalDate): List<NutritionValues>
+//
+//    // Get product diary entries' nutrition values by date
+//    @Query("SELECT nutritionValues FROM ProductDiaryEntry WHERE date = :date")
+//    suspend fun getProductDiaryEntriesNutritionValues(date: LocalDate): List<NutritionValues>
 
     // Get user recipes
     @Query("""
         SELECT * FROM Recipe
-        WHERE (:userId IS NULL OR user_id = :userId)
+        WHERE (:userId IS NULL OR userId = :userId)
         AND (:searchText IS NULL OR name LIKE '%' || :searchText || '%')
         ORDER BY creationDateTime DESC LIMIT :limit OFFSET :offset
     """)
-    suspend fun getUserRecipes(userId: String?, searchText: String?, limit: Int, offset: Int): List<Recipe>
+    fun getUserRecipes(userId: String?, searchText: String?, limit: Long, offset: Long): Flow<List<Recipe>>
 
     // Insert or replace a recipe
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertRecipe(recipe: Recipe)
+
+    // Insert or replace a recipe
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipes(recipe: List<Recipe>)
 
     // Get recipe by ID
     @Query("SELECT * FROM Recipe WHERE id = :recipeId LIMIT 1")
@@ -85,24 +91,28 @@ interface DiaryDao {
 
     // Insert single recipe diary entry
     @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipeDiaryEntries(recipeDiaryEntries: List<RecipeDiaryEntry>)
+
+    // Insert single recipe diary entry
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertRecipeDiaryEntry(entry: RecipeDiaryEntry)
 
     // Get recipe diary entries with search
     @Query("""
-        SELECT * FROM RecipeDiaryEntry
-        WHERE (:searchText IS NULL OR recipe_name LIKE '%' || :searchText || '%')
-        GROUP BY recipe_id
+        SELECT * FROM recipeDiaryEntry
+        WHERE (:searchText IS NULL OR recipeName LIKE '%' || :searchText || '%')
+        GROUP BY recipeId
         ORDER BY creationDateTime DESC LIMIT :limit OFFSET :offset
     """)
     suspend fun getRecipeDiaryEntries(searchText: String?, limit: Int, offset: Int): List<RecipeDiaryEntry>
 
     // Get all recipe diary entries
     @Query("SELECT * FROM RecipeDiaryEntry ORDER BY creationDateTime DESC LIMIT :limit")
-    suspend fun getAllRecipeDiaryEntries(limit: Int): List<RecipeDiaryEntry>
+    fun getAllRecipeDiaryEntries(limit: Long): Flow<List<RecipeDiaryEntry>>
 
     // Get recipe diary entries by date
     @Query("SELECT * FROM RecipeDiaryEntry WHERE date = :date ORDER BY creationDateTime DESC")
-    suspend fun getRecipeDiaryEntriesByDate(date: LocalDate): Flow<List<RecipeDiaryEntry>>
+    fun getRecipeDiaryEntriesByDate(date: LocalDate): Flow<List<RecipeDiaryEntry>>
 
     // Delete a recipe diary entry
     @Delete
@@ -112,7 +122,7 @@ interface DiaryDao {
     @Query("DELETE FROM RecipeDiaryEntry WHERE date = :date")
     suspend fun deleteRecipeDiaryEntriesByDate(date: LocalDate)
 
-    // Get recipe diary entries' nutrition values by date
-    @Query("SELECT nutrition_values FROM RecipeDiaryEntry WHERE date = :date")
-    suspend fun getRecipeDiaryEntriesNutritionValues(date: LocalDate): List<NutritionValues>
+//    // Get recipe diary entries' nutrition values by date
+//    @Query("SELECT nutrition_values FROM RecipeDiaryEntry WHERE date = :date")
+//    suspend fun getRecipeDiaryEntriesNutritionValues(date: LocalDate): List<NutritionValues>
 }
