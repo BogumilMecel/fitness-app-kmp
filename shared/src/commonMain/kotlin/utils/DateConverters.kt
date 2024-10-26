dd35e014772566fd0680e2e7d56c16f0bf2e31d7
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-26 16:01:02 +0200

    Add date converters

---
+++ shared/src/commonMain/kotlin/utils/DateConverters.kt
@@ -0,0 +1,25 @@
+package utils
+
+import androidx.room.TypeConverter
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class LocalDateConverter {
+
+    @TypeConverter
+    fun fromLocalDate(value: LocalDate) = Json.encodeToString(value)
+
+    @TypeConverter
+    fun toLocalDate(value: String): LocalDate = Json.decodeFromString(value)
+}
+
+class LocalDateTimeConverter {
+
+    @TypeConverter
+    fun fromLocalDate(value: LocalDateTime) = Json.encodeToString(value)
+
+    @TypeConverter
+    fun toLocalDate(value: String): LocalDateTime = Json.decodeFromString(value)
+}
