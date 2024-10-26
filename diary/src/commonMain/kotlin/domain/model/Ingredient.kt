3edca80544ed8f2c88cea741247a4aad5660d12e
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-26 17:17:03 +0200

    Add ingredients type converter

--- diary/src/commonMain/kotlin/domain/model/Ingredient.kt
+++ diary/src/commonMain/kotlin/domain/model/Ingredient.kt
@@ -1,6 +1,9 @@
 package domain.model
 
+import androidx.room.TypeConverter
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 @Serializable
 data class Ingredient(
@@ -10,3 +13,12 @@ data class Ingredient(
     val productId: String,
     val measurementUnit: MeasurementUnit,
 )
+
+class IngredientsConverter {
+
+    @TypeConverter
+    fun fromIngredientsList(value: List<Ingredient>) = Json.encodeToString(value)
+
+    @TypeConverter
+    fun toIngredientsList(value: String): List<Ingredient> = Json.decodeFromString(value)
+}
