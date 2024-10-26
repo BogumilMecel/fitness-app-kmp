612e64eeffb7413455cddafa89f4b26c26961574
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-10-26 17:17:17 +0200

    Add ingredients type converter to database

--- composeApp/src/commonMain/kotlin/data/database/FitnessAppDatabase.kt
+++ composeApp/src/commonMain/kotlin/data/database/FitnessAppDatabase.kt
@@ -4,17 +4,20 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import data.api.DiaryDao
+import domain.model.IngredientsConverter
 import domain.model.NutritionValuesTypeConverter
 import domain.model.Product
 import domain.model.ProductDiaryEntry
 import domain.model.Recipe
 import domain.model.RecipeDiaryEntry
+import utils.LocalDateConverter
+import utils.LocalDateTimeConverter
 
 @Database(
     entities = [Product::class, Recipe::class, ProductDiaryEntry::class, RecipeDiaryEntry::class],
     version = 1,
 )
-@TypeConverters(value = [NutritionValuesTypeConverter::class])
+@TypeConverters(value = [NutritionValuesTypeConverter::class, LocalDateConverter::class, LocalDateTimeConverter::class, IngredientsConverter::class])
 abstract class FitnessAppDatabase : RoomDatabase() {
 
     companion object {
