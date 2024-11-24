91bd9bb0be8d3944e2cf7ee29b33b481d7dc5d56
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-11-24 11:08:06 +0100

    add paging constants

--- utils/src/commonMain/kotlin/constans/Constants.kt
+++ utils/src/commonMain/kotlin/constans/Constants.kt
@@ -9,4 +9,8 @@ object Constants {
     object Query {
         const val LATEST_DATE_TIME = "latest_date_time"
     }
+    object Paging {
+        const val API_PAGE_SIZE = 20L
+        const val OFFLINE_PAGE_SIZE = 100L
+    }
 }
