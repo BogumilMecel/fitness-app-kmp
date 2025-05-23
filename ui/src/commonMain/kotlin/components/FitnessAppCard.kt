ee0c35f83c866abc5ee61f359743a90a3e36019c
Bogumil Mecel <bogumilmecel2@gmail.com>
2025-05-23 07:30:20 +0200

    Migrate card to material 3

--- ui/src/commonMain/kotlin/components/FitnessAppCard.kt
+++ ui/src/commonMain/kotlin/components/FitnessAppCard.kt
@@ -1,6 +1,7 @@
 package components
 
-import androidx.compose.material.Card
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import theme.FitnessAppTheme
@@ -9,7 +10,7 @@ import utils.defaultRoundedCornerShape
 @Composable
 fun FitnessAppCard(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
     Card(
-        backgroundColor = FitnessAppTheme.colors.surface,
+        colors = CardDefaults.cardColors().copy(containerColor = FitnessAppTheme.colors.surface),
         shape = defaultRoundedCornerShape,
         modifier = modifier
     ) {
