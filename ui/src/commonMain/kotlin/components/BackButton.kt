d4226de3f495d06c3c5ee2ae8c7e1191b1e976bb
bogumil.mecel <bogumilmecel2@gmail.com>
2025-08-01 21:48:20 +0200

    Add modifier to back button

--- ui/src/commonMain/kotlin/components/BackButton.kt
+++ ui/src/commonMain/kotlin/components/BackButton.kt
@@ -1,19 +1,20 @@
 package components
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
-fun BackButton(onClick: () -> Unit) {
+fun BackButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
     IconButton(
         onClick = onClick,
-        modifier = Modifier.padding(vertical = 16.dp)
+        modifier = modifier,
     ) {
         Icon(
             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
